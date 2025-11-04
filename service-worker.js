ed14f5cc326a003eb6b2c9c5f7590a4dc71aea6a
lakshyaconsultancymks-design <lakshya.consultancymks@gmail.com>
2025-11-04 16:29:32 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,35 @@
-const CACHE_NAME = 'calculator-pwa-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
+const CACHE_NAME = "pwa-calculator-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-// Install event - cache files
-self.addEventListener('install', event => {
+// Install service worker
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  console.log("Service Worker Installed");
+});
+
+// Fetch from cache first
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
 
-// Activate event - cleanup old caches
-self.addEventListener('activate', event => {
+// Activate and clean old caches
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
     )
   );
-});
-
-// Fetch event - serve cached content when offline
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+  console.log("Service Worker Activated");
 });
